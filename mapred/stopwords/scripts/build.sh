86fd7789ee97de36ec6be1de7930f1b3cb4849e6
cds <cds@utk.edu>
2014-03-13 17:57:02 -0400

    Updated scripts to reflect directory structure

--- mapred/stopwords/scripts/build.sh
+++ mapred/stopwords/scripts/build.sh
@@ -1,2 +1,2 @@
-javac -classpath ~/hdc/hadoop-core-1.2.1.jar -d filterandwordcount_classes FilterAndWordCount.java
-jar -cvf filterandwordcount.jar -C filterandwordcount_classes/ .
+javac -classpath ~/hdc/hadoop-core-1.2.1.jar -d ../src/filterandwordcount_classes ../src/FilterAndWordCount.java
+jar -cvf ../src/filterandwordcount.jar -C ../src/filterandwordcount_classes/ .
--- mapred/stopwords/scripts/run.sh
+++ mapred/stopwords/scripts/run.sh
@@ -1,7 +1,7 @@
 hadoop fs -mkdir /user/cslater3/input
-hadoop fs -put ./input/* /user/cslater3/input
+hadoop fs -put ../input/* /user/cslater3/input
 hadoop fs -rmr /user/cslater3/output
-hadoop jar filterandwordcount.jar edu.utk.eecs.FilterAndWordCount /user/cslater3/input/* /user/cslater3/output
-hadoop fs -get /user/cslater3/output/part-00000
-python generate_stopwords.py part-00000 > stopwords.txt
+hadoop jar ../src/filterandwordcount.jar edu.utk.eecs.FilterAndWordCount /user/cslater3/input/* /user/cslater3/output
+hadoop fs -get /user/cslater3/output/part-00000 ../output
+python ../src/generate_stopwords.py ../output/part-00000 > stopwords.txt
 hadoop fs -rmr /user/cslater3/input
