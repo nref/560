7e911d92154ba0a6bac3a6a96627fe9e708085c6
Carl Douglas Slater <cslater3@hydra29.eecs.utk.edu>
2014-03-13 16:49:12 -0400

    Added clean_hdfs.sh, removes some clutter

---
+++ mapred/clean_hdfs.sh
@@ -0,0 +1,5 @@
+ssh cslater3@hydra29.eecs.utk.edu rm -rf /local_scratch/cslater3/hadoop/*
+ssh cslater3@hydra28.eecs.utk.edu rm -rf /local_scratch/cslater3/hadoop/*
+ssh cslater3@hydra27.eecs.utk.edu rm -rf /local_scratch/cslater3/hadoop/*
+ssh cslater3@hydra26.eecs.utk.edu rm -rf /local_scratch/cslater3/hadoop/*
+ssh cslater3@hydra25.eecs.utk.edu rm -rf /local_scratch/cslater3/hadoop/*
--- mapred/document_filter/run.sh
+++ mapred/document_filter/run.sh
@@ -1,5 +1,7 @@
 hadoop fs -mkdir /user/cslater3/input
 hadoop fs -put ./input/* /user/cslater3/input
 hadoop fs -rmr /user/cslater3/output
-hadoop jar filterandwordcount.jar edu.utk.eecs.FilterAndWordCount /user/cslater3/input/* /user/cslater3/output > stopwords.txt
+hadoop jar filterandwordcount.jar edu.utk.eecs.FilterAndWordCount /user/cslater3/input/* /user/cslater3/output
+hadoop fs -get /user/cslater3/output/part-00000
+python generate_stopwords.py part-00000 > stopwords.txt
 hadoop fs -rmr /user/cslater3/input
