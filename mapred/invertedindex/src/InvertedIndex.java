19c691259cc6f60a6e32d641b7f31faf0cf0a77c
cds <cds@utk.edu>
2014-03-21 17:13:45 -0400

    Got InvertedIndex.java to compile, still has type cast issues

--- mapred/invertedindex/src/InvertedIndex.java
+++ mapred/invertedindex/src/InvertedIndex.java
@@ -7,25 +7,26 @@ import org.apache.hadoop.fs.*;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
+//import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.*;
 import org.apache.hadoop.util.*;
 
 /*
  */
 public class InvertedIndex {
-
 	
 	public static class Map extends MapReduceBase implements Mapper<Text, Text, Text, Text> {
-        //private final static IntWritable one = new IntWritable(1);
+        
         private Text word = new Text();
 		private Text docID = new Text();
-
+        
         public void map(Text key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
             
             String line = value.toString();
-			String lineNum = line.substring(0,a.indexOf(' ')); //now I'm keeping it as a string instead of parseInt
-			String remainder = line.substring(a.indexOf(' ')).trim();
+			String lineNum = line.substring(0, line.indexOf(' '));
+			String remainder = line.substring(line.indexOf(' ')).trim();
 
-			FileSplit fileSplit = (FileSplit)context.getInputSplit();
+			FileSplit fileSplit = (FileSplit)reporter.getInputSplit();
 			String filename = fileSplit.getPath().getName();
 			docID.set(filename+":"+lineNum);
 			
@@ -35,21 +36,18 @@ public class InvertedIndex {
                 
                 word.set(tokenizer.nextToken());
                 
-                // Split this word on all punctuation characters
-                // Also trim leading and trailing whitespace
-                // Also make lower-case
                 String clean = word.toString().replaceAll("[\\p{P}]", " ").trim().toLowerCase();
                 List<String> parts = new ArrayList<String>(Arrays.asList(clean.split(" ")));
 
                 // If there is only one word in the split
-				// TODO: Remember to filter out stopwords from the previous 
-				//       mapreducer
-                if (parts.size() == 1) {
+				// TODO: filter stopwords
+                if (1 == parts.size()) {
                     word.set(clean);
                     output.collect(word, docID);
                 }
+                
                 // Map the split words
-                else{
+                else {
                     for (String part : parts) {
                         Text part_Text = new Text(part);
                         map(key, part_Text, output, reporter);
@@ -71,14 +69,14 @@ public class InvertedIndex {
     public static void main(String[] args) throws Exception {
 			
 		JobConf conf = new JobConf(InvertedIndex.class);
-		conf.setJobName("FilterAndWordCount");
+		conf.setJobName("InvertedIndex");
 		
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(Text.class);
+//		conf.setOutputKeyClass(Text.class);
+//		conf.setOutputValueClass(Text.class);
 		
 		conf.setMapperClass(Map.class);
-		conf.setCombinerClass(Reduce.class);
-		conf.setReducerClass(Reduce.class);
+//		conf.setCombinerClass(Reduce.class);
+//		conf.setReducerClass(Reduce.class);
 		
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
