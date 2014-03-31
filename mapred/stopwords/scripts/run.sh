9dc8669adbe571ebf765584975025463dfa6097e
slater1 <cds@utk.edu>
2014-03-31 12:29:01 -0400

    removed absolute paths to ccraig7

--- mapred/stopwords/scripts/run.sh
+++ mapred/stopwords/scripts/run.sh
@@ -1,8 +1,8 @@
-hadoop fs -mkdir /user/ccraig7/input
-hadoop fs -put ../input/* /user/ccraig7/input
-hadoop fs -rmr /user/ccraig7/output
-hadoop jar ../src/filterandwordcount.jar edu.utk.eecs.FilterAndWordCount /user/ccraig7/input/* /user/ccraig7/output
+hadoop fs -mkdir input
+hadoop fs -put ../input/* input
+hadoop fs -rmr output
+hadoop jar ../src/filterandwordcount.jar edu.utk.eecs.FilterAndWordCount input/* output
 rm ../output/part-00000
-hadoop fs -get /user/ccraig7/output/part-00000 ../output
+hadoop fs -get output/part-00000 ../output
 python ../src/generate_stopwords.py ../output/part-00000 > ../output/stopwords.txt
-hadoop fs -rmr /user/ccraig7/input
+hadoop fs -rmr input
