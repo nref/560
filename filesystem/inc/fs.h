03705d1ae3bd7cd3a99006e5d335fd9a2269482d
cds <cds@utk.edu>
2014-04-03 22:51:38 -0400

    Added structs for file, inode, dirent, etc.

--- filesystem/inc/fs.h
+++ filesystem/inc/fs.h
@@ -1,6 +1,55 @@
-#define BLKSIZE 4096	// Block size in bytes
-#define NBLOCKS 25600	// Max num allocatable blocks
-                        // 4096 bytes * 25600 blocks == 100MB
+#define BLKSIZE 4096					// Block size in bytes
+#define MAXBLOCKS 25600					// Max num allocatable blocks. 4096 bytes * 25600 blocks == 100MB
+#define NBLOCKS 10						// Number of direct blocks a block can point to
+#define NIBLOCKS 10						// Number of indirect blocks a block can point to
+
+#define FS_READ 0
+#define FS_WRITE 1
+
+#define FS_FILE 0
+#define FS_DIR 1
+#define FS_LINK 2
+
+typedef unsigned long fs_ino_t;			// Inode number is just a ulong
+typedef unsigned int fs_mode_t;			// File mode is just an int ('r', 'w', etc)
+
+typedef struct block {
+	char data[BLKSIZE];
+} block;
+
+typedef struct iblock {	
+
+	block* blocks[NBLOCKS];
+	struct iblock* iblocks;				// nth-level indirect blocks
+} iblock;
+
+typedef struct dirent {					// Directory entry
+	fs_ino_t inode;						// inode number
+	char name[];						// filename
+} dirent;
+
+typedef struct inode {
+	fs_ino_t num;						// Inode number
+	int nlinks;							// Number of hard links to the inode
+	int size;							// File size
+	int mode;							// 0 file, 1 directory, 2 link
+
+	block* blocks[NBLOCKS];		// Direct blocks
+	iblock* iblocks[NIBLOCKS];	// First-level indirect blocks
+} inode;
+
+typedef struct file {
+	fs_mode_t mode;						// 0 or 'r' read, 1 or 'w' write
+	int f_pos;							// Byte offset seek'ed to
+	inode* f_inode;				// pointer to the file's inode
+} file;
+
+typedef struct filesystem {	
+	struct block blocks[MAXBLOCKS];
+
+	struct block* superblock;			// Block 0
+	struct block* free_block_bitmap;	// Block 1. A map of free blocks
+} filesystem;
 
 extern int fs_mkfs();
 extern int fs_open(char* filename, char* mode);
--- filesystem/src/fs.c
+++ filesystem/src/fs.c
@@ -9,41 +9,56 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "fs.h"
 
 #if defined(_WIN64) || defined(_WIN32)
 #include <windows.h>
 #endif
 
-static char* file = "fs";
+static char* fname = "fs";
 
 int fs_mkfs() {
-   	int i, fp_no, ERR;
-	char out[BLKSIZE];
-	FILE* fp = fopen(file, "w");
+   	int i, ERR;
+	char buf[BLKSIZE];
+	FILE* fp = fopen(fname, "w");
+	filesystem* fs;
 
 /* Preallocate a contiguous file. Differs across platforms */
 #if defined(_WIN64) || defined(_WIN32)
 	LARGE_INTEGER offset;
-	offset.QuadPart = BLKSIZE*NBLOCKS;
+	offset.QuadPart = BLKSIZE*MAXBLOCKS;
 	ERR = SetFilePointerEx(fp, offset, NULL, FILE_BEGIN);
 	SetEndOfFile(fp);
 #elif __APPLE__	/* __MACH__ also works */
-    fstore_t store = {F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, BLKSIZE*NBLOCKS};
+    fstore_t store = {F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, BLKSIZE*MAXBLOCKS};
     ERR = fcntl(fileno(fp), F_PREALLOCATE, &store);
 #elif __unix__
-    ERR = posix_fallocate(fp, 0, BLKSIZE*NBLOCKS);
+    ERR = posix_fallocate(fp, 0, BLKSIZE*MAXBLOCKS);
 #endif
     
 	if (ERR < 0) { 
 		perror("allocation error");
 		return ERR;
 	}
-	for (i = 0; i < NBLOCKS; i++) {
+
+	fs = (filesystem*)malloc(sizeof(filesystem));
+	fs->superblock = &fs->blocks[0];
+	fs->free_block_bitmap = &fs->blocks[1];
+
+	memset(fs->superblock->data, 0, BLKSIZE);				// Zero-out the free block
+	memset(fs->free_block_bitmap->data, 0, BLKSIZE);		// Zero-out the free block
+
+	for (i = 0; i < MAXBLOCKS; i++) {
 		fseek(fp, BLKSIZE*i, SEEK_SET);
 
-        sprintf(out, "-block %d-", i);
-		fputs(out, fp);
+		if (i==0) fputs(fs->superblock->data, fp);
+		else if (i==1) fputs(fs->free_block_bitmap->data, fp);
+		else {
+			sprintf(buf, "-block %d-", i);
+			fputs(buf, fp);
+		}
 	}
 
 	return 0;
